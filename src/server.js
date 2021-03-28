a0a07601989fb945925b676f88ac939855420b9d
venturaf <franner.ventura@gmail.com>
2021-03-28 19:48:05 -0300

    [dr-front-v9] subiendo a heroku
    [dr-front-v9] subiendo a heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'src/index.html'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'src/index.html'));
+    
 });
 
 app.listen(process.env.PORT || 8080);
